bf591c448b3d54ab681df9d1ca7bf2e16a8ddc95
Vipul Tyagi <vipultyagi52444@gmail.com>
2024-09-15 15:15:36 +0530

    URL update in authStore.js

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -2,7 +2,7 @@ import React from "react";
 import { create } from "zustand";
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/auth";
+const API_URL = import.meta.env.MODE === "development"?"http://localhost:5000/api/auth":"/api/auth";
 
 axios.defaults.withCredentials = true;
 
